93b390c080fe78a93fbdb2e27ddc28b1b028f126
Bridget Bangert <bridgetmbangert@gmail.com>
2025-03-21 23:57:28 -0500

    Upload and Output Dirs

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,9 @@ app.add_middleware(
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
 OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
+
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 @app.get("/")
 async def root():
